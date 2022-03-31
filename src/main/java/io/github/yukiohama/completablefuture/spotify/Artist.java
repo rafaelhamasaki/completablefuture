8bc51721d5b14820e18debffe800fc2c43e5e492
Rafael Yukio Hamasaki <rafael.hamasaki@ebanx.com>
2022-03-31 11:27:45 -0300

    Removed artist id from http response

--- src/main/java/io/github/yukiohama/completablefuture/spotify/Artist.java
+++ src/main/java/io/github/yukiohama/completablefuture/spotify/Artist.java
@@ -2,12 +2,17 @@ package io.github.yukiohama.completablefuture.spotify;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 public class Artist {
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String id;
+
     private String name;
 
     @Setter
